e98b47f4795e1d5a3e91edf9b1eedd4b20cf1703
ajstylesjonathan@gmail.com <https:/github.com/Ajstyles81/house.git>
2026-07-19 03:11:47 +0100

    modified

--- grade.py
+++ grade.py
@@ -9,23 +9,23 @@ if first_grade >= 90: grade1 = 'A'
 elif first_grade >= 80: grade1 = 'B'
 elif first_grade >= 70: grade1 = 'C'
 elif first_grade >= 60: grade1 = 'D'
-elif first_grade >= 0: grade1 = 'f'
+else: grade1 = 'F'
 
-if first_grade >= 90: grade2 = 'A'
+if second_grade >= 90: grade2 = 'A'
 elif second_grade >= 80: grade2 = 'B'
 elif second_grade >= 70: grade2 = 'C'
 elif second_grade >= 60: grade2 = 'D'
-elif second_grade >= 0: grade2 = 'f'
+else: grade2 = 'F'
 
-if first_grade >= 90: grade3 = 'A'
-elif second_grade >= 80: grade3 = 'B'
-elif second_grade >= 70: grade3 = 'C'
-elif second_grade >= 60: grade3 = 'D'
-elif second_grade >= 0: grade3 = 'f'
+if third_grade >= 90: grade3 = 'A'
+elif third_grade >= 80: grade3 = 'B'
+elif third_grade >= 70: grade3 = 'C'
+elif third_grade >= 60: grade3 = 'D'
+else: grade3 = 'F'
 
 
-
-print(grade1)
-print(grade2)
-print(grade3)
+print()
+print("First grade Entered" ,grade1)
+print("Second grade Entered" ,grade2)
+print("Third grade Entered" ,grade3)
 print ("The total Average of Score inputed is:" , Sum)
