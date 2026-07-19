62d6d61d2cf13617409240698432ec8fc95c830f
ajstylesjonathan@gmail.com <https:/github.com/Ajstyles81/house.git>
2026-07-19 19:01:08 +0100

    Exercise commit

---
+++ Exercise/AgeCalculator.py
@@ -0,0 +1,8 @@
+Name = (input("Your Name: "))
+Age = int(input("Enter Age: "))
+
+AgePlus = Age + 5
+AgeMinus = Age - 5
+
+print(Name,"will be", AgePlus,"years old in five years time.") 
+print(Name,"was", AgeMinus,"years old five years ago.")  
---
+++ Vowels.py
@@ -0,0 +1,12 @@
+First_Integer = int(input("Enter First Integer: "))
+Second_Integer = int(input("Enter Second Integer: "))
+Third_Integer = int(input("Enter Third Integer: "))
+
+if Second_Integer >= First_Integer:
+	print ("Second Integer is the Largest")
+
+if Third_Integer >= Second_Integer :
+	print ("Third Integer is the largest") 
+
+if First_Integer >= Third_Integer :
+	print ("First Integer is the largest") 
