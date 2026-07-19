56ef96591477cd5e4017225c11f8192eb6c21569
ajstylesjonathan@gmail.com <https:/github.com/Ajstyles81/house.git>
2026-07-19 21:33:55 +0100

    AdultChild

---
+++ Exercise/ChildAdult.py
@@ -0,0 +1,6 @@
+Name = (input("Enter your name: "))
+Age = int(input("Enter Age: "))
+
+if Age <= 12: print (Name, "is a child")
+elif Age >= 13 and Age <= 18: print (Name, "is a Tennager")
+elif Age >= 19: print (Name, "is an Adult")
