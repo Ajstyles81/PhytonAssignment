e11d6841d4f091b9241efa1c93053c23865c7c49
ajstylesjonathan@gmail.com <https:/github.com/Ajstyles81/house.git>
2026-07-19 19:34:29 +0100

    7:30

---
+++ Exercise/andb.py
@@ -0,0 +1,22 @@
+print("a,   b,	  pow(a, b)")
+
+a = 1
+b = 2
+print(a, b , pow(a, b), sep="    ")
+
+a=2
+b=3
+print(a, b, pow(a, b), sep="    ")
+
+a = 3
+b = 4
+print(a, b, pow(a, b), sep="    ")
+
+a = 4
+b = 5
+print(a, b, pow(a, b), sep="    ")
+
+a = 5
+b = 6
+print(a, b, pow(a, b), sep="    ")
+
