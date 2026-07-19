31028ee8a2b58cc9d2c887af1330fe7c587cfda3
ajstylesjonathan@gmail.com <https:/github.com/Ajstyles81/house.git>
2026-07-19 21:19:07 +0100

    WeekExecise commit

---
+++ Exercise/Week.py
@@ -0,0 +1,17 @@
+DayNumber = int(input("Enter a Number: "))
+Day1 = "Sunday"
+Day2 = "Monday"
+Day3 = "Tuesday"
+Day4 = "Wednesday" 
+Day5 = "Thursday" 
+Day6 = "Friday" 
+Day7 = "Saturday"
+
+if DayNumber == 1: print("You entered",Day1)
+elif DayNumber == 2: print("You entered",Day2)
+elif DayNumber == 3: print("You entered",Day3)
+elif DayNumber == 4: print("You entered",Day4)
+elif DayNumber == 5: print("You entered",Day5)
+elif DayNumber == 6: print("You entered",Day6)
+elif DayNumber == 7: print("You entered",Day7)
+else: print("Invalid day")
